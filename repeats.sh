362828aa5b17b4410750a2a58f5046a0173b26de
Jiang Yuxin 姜宇心 <70904956+YJiangcm@users.noreply.github.com>
2023-01-10 01:07:44 +0800

    Update repeats.sh

--- repeats.sh
+++ repeats.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-python3 run.py --seed 0 --cuda 1 --num_gcn_layer 2
+python3 run.py --seed 0 --cuda 0 --num_gcn_layer 2
 sleep 1m
-python3 run.py --seed 1 --cuda 1 --num_gcn_layer 2
+python3 run.py --seed 1 --cuda 0 --num_gcn_layer 2
 sleep 1m
-python3 run.py --seed 2 --cuda 1 --num_gcn_layer 2
+python3 run.py --seed 2 --cuda 0 --num_gcn_layer 2
 sleep 1m
-python3 run.py --seed 3 --cuda 1 --num_gcn_layer 2
+python3 run.py --seed 3 --cuda 0 --num_gcn_layer 2
 sleep 1m
-python3 run.py --seed 4 --cuda 1 --num_gcn_layer 2
+python3 run.py --seed 4 --cuda 0 --num_gcn_layer 2
